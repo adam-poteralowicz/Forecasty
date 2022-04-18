04d2935287e5a2ee86095069d163bbe2bae00448
apoteralowicz <adam.poteralowicz@gmail.com>
2022-04-18 08:16:13 +0200

    Display location and temperature

--- app/src/main/java/com/apap/forecasty/domain/model/Forecast.kt
+++ app/src/main/java/com/apap/forecasty/domain/model/Forecast.kt
@@ -1,6 +1,7 @@
 package com.apap.forecasty.domain.model
 
 import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -14,4 +15,11 @@ data class Forecast(
     val hourlyForecast: List<HourlyForecast> = emptyList(),
     val dailyForecast: List<DailyForecast> = emptyList(),
     val weatherAlerts: List<WeatherAlert> = emptyList(),
-) : Parcelable
+) : Parcelable {
+
+    @IgnoredOnParcel
+    val currentConditions = currentForecast.weather[0].main
+
+    @IgnoredOnParcel
+    val currentTemperature = currentForecast.temperature.toString()
+}
--- app/src/main/java/com/apap/forecasty/presentation/view/WeatherScreen.kt
+++ app/src/main/java/com/apap/forecasty/presentation/view/WeatherScreen.kt
@@ -6,19 +6,27 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.ExperimentalUnitApi
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.apap.forecasty.R
 import com.apap.forecasty.domain.model.Forecast
 import com.apap.forecasty.ui.theme.ForecastyBlue
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
@@ -37,29 +45,36 @@ fun CurrentWeather(
     isLightTheme: Boolean,
 ) {
     forecast ?: return
-    val currentForecast = forecast.currentForecast
-    val currentHour = LocalDateTime.now().hour
-    val isNight: Boolean = currentHour < 6 || currentHour > 18
+    val currentDate = LocalDateTime.now()
+    val isNight: Boolean = currentDate.hour < 6 || currentDate.hour > 18
 
-    Box(modifier = Modifier
+    Column(modifier = Modifier
         .background(if (isLightTheme) ForecastyBlue else Color.Black)
-        .wrapContentSize(align = Alignment.Center)
-        .padding(4.dp)) {
+        .fillMaxSize()
+        .padding(4.dp)
+    ) {
+        Toolbar(isLightTheme)
         Column(
-            modifier = Modifier.padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+                .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
+            DateAndLocation(
+                currentDate.format(DateTimeFormatter.ISO_DATE).toString(),
+                forecast.timezone,
+                isLightTheme
+            )
             WeatherImage(
-                modifier = Modifier.scale(5f),
-                image = when (currentForecast.weather[0].main) {
+                modifier = Modifier.fillMaxWidth().scale(5f),
+                imageResId = when (forecast.currentConditions) {
                     "Clear" -> if (isNight) R.drawable.ic_night else R.drawable.ic_day
                     "Clouds" -> R.drawable.ic_cloudy
                     "Rain" -> R.drawable.ic_rainy
                     "Snow" -> R.drawable.ic_snowy
                     else -> R.drawable.ic_day
                 },
-                contentDescription = "weather_image",
             )
+            TemperatureText(forecast.currentTemperature, isLightTheme)
         }
     }
 }
@@ -67,13 +82,85 @@ fun CurrentWeather(
 @Composable
 fun WeatherImage(
     modifier: Modifier = Modifier,
-    image: Int,
-    contentDescription: String,
+    imageResId: Int,
 ) {
-    val paintImage: Painter = painterResource(id = image)
+    val paintImage: Painter = painterResource(id = imageResId)
     Image(
         modifier = modifier,
         painter = paintImage,
-        contentDescription = contentDescription,
+        contentDescription = "weather_image",
     )
+}
+
+@Composable
+fun DateAndLocation(
+    date: String,
+    location: String,
+    isLightTheme: Boolean,
+) {
+    Column(
+        modifier = Modifier
+            .wrapContentSize(align = Alignment.TopStart)
+            .padding(16.dp)
+    ) {
+        Text(
+            text = date,
+            fontWeight = FontWeight.Medium,
+            color = if (isLightTheme) Color.White else ForecastyBlue,
+            style = MaterialTheme.typography.titleMedium,
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Row(modifier = Modifier.wrapContentSize(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            LocationImage(
+                imageResId = R.drawable.ic_location,
+                modifier = Modifier
+                    .size(32.dp)
+                    .padding(end = 8.dp)
+            )
+            Text(
+                text = location,
+                fontWeight = FontWeight.Bold,
+                color = if (isLightTheme) Color.White else ForecastyBlue,
+                style = MaterialTheme.typography.titleLarge,
+            )
+        }
+    }
+}
+
+@Composable
+fun LocationImage(
+    imageResId: Int,
+    modifier: Modifier = Modifier,
+) {
+
+    val paintImage: Painter = painterResource(id = imageResId)
+    Image(
+        modifier = modifier,
+        painter = paintImage,
+        contentScale = ContentScale.Fit,
+        contentDescription = "location_image",
+    )
+}
+
+@OptIn(ExperimentalUnitApi::class)
+@Composable
+fun TemperatureText(
+    temperature: String,
+    isLightTheme: Boolean,
+) {
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = "$temperature °C",
+            Modifier.padding(PaddingValues(bottom = 75.dp)),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = TextUnit(value = 48f, type = TextUnitType.Sp),
+            color = if (isLightTheme) Color.White else ForecastyBlue,
+        )
+    }
 }
